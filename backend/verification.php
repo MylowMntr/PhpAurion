c946693f69ab8345a00009ff86f2d3852a6ffaac
MiloMontuori <milo.montuori@student.junia.com>
2022-04-05 15:19:47 +0200

    up verif/coDB

--- backend/verification.php
+++ backend/verification.php
@@ -3,23 +3,18 @@ session_start();
 if(isset($_POST['username']) && isset($_POST['password']))
 {
     // connexion à la base de données
-    $hostname="localhost"; //nom du serveur (localhost)
-    $username="root";//nom d'utilisateur pour accéder au serveur (root)
-    $password="root"; //mot de passe pour accéder au serveur (root)
-    $dbname="ecole"; //nom de la base de données
-    $db = mysqli_connect($db_host, $db_username, $db_password,$db_name)
-           or die('could not connect to database');
+    require('connexion.php');
     
     // on applique les deux fonctions mysqli_real_escape_string et htmlspecialchars
     // pour éliminer toute attaque de type injection SQL et XSS
-    $username = mysqli_real_escape_string($db,htmlspecialchars($_POST['username'])); 
-    $password = mysqli_real_escape_string($db,htmlspecialchars($_POST['password']));
+    $username = mysqli_real_escape_string($connexion,htmlspecialchars($_POST['username'])); 
+    $password = mysqli_real_escape_string($connexion,htmlspecialchars($_POST['password']));
     
     if($username !== "" && $password !== "")
     {
         $requete = "SELECT count(*) FROM utilisateur where 
               nom_utilisateur = '".$username."' and mot_de_passe = '".$password."' ";
-        $exec_requete = mysqli_query($db,$requete);
+        $exec_requete = mysqli_query($connexion,$requete);
         $reponse      = mysqli_fetch_array($exec_requete);
         $count = $reponse['count(*)'];
         if($count!=0) // nom d'utilisateur et mot de passe correctes
@@ -41,5 +36,5 @@ else
 {
    header('Location: connexion.php');
 }
-mysqli_close($db); // fermer la connexion
+mysqli_close($connexion); // fermer la connexion
 ?>
