73c8aa580be9bfa66843331bc623b24b271d0c68
MiloMontuori <milo.montuori@student.junia.com>
2022-05-05 16:48:36 +0200

    up note+abs

--- absences.php
+++ absences.php
@@ -12,6 +12,42 @@
 
 <div class="container" id="main-content">
 	<h2>Absences</h2>
+	<?php
+		require("./backend/connectDB.php");
+		// $request = "SELECT * FROM absences INNER JOIN matiere ON  matiere.matiereID = absences.idMatiere WHERE idEleve = '".$_SESSION['id']."' "; 
+		$request = "SELECT * FROM absences INNER JOIN matiere ON  matiere.matiereID = absences.idMatiere WHERE idEleve = '4' "; 
+
+		$resultat =mysqli_query($connexion,$request); //Executer la requete	  
+		if(mysqli_num_rows($resultat) > 0)  
+		{ ?>
+			<table class="table table-hover">
+			<thead>
+				<tr>
+					<th scope="col">Matiere</th>
+					<th scope="col">Date</th>
+					<th scope="col">Durée d'absence</th>
+				</tr>
+			</thead>
+			<?php
+				while($row = mysqli_fetch_array($resultat)){  
+			?>  
+				<tr>  
+					<th scope="col"><?php echo $row["nomMatiere"];?></th>  
+					<th scope="col"><?php echo date('d-m-Y', strtotime($row["date"])) ; ?></th>
+					<th scope="col"><?php echo $row["duree"]."h"; ?></th>
+				</tr>  
+			<?php  
+				}  
+			?>
+			</table>
+
+
+		<?php
+		}
+		else{
+			echo "<p> Vous n'avez pas d'absence. </p>";
+		} 
+		?> 	
 </div>
 
 <?php include("includes/footer.php");?>
