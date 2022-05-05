3a90aae49f22fd65bdc75558385c8debebbd019f
SachaEvn <sacha.evain@student.junia.com>
2022-05-05 16:48:08 +0200

    db tout ça

--- ajouterNote.php
+++ ajouterNote.php
@@ -10,33 +10,32 @@
 <?php include("includes/top.php");?>
 
 <?php include("includes/navigation.php");?>
-<?php 
-if(isset($_SESSION['prof']) && ($_SESSION['prof'] == 1)){
-include("includes/mainconfig.php");?>
-
 
 <?php 
 if(isset($_SESSION["prof"]) && $_SESSION["prof"] == 1){
 include("includes/mainconfig.php");?>
+<?php include('backend/interactDB.php'); ?>
 
 
 <div class="container" id="main-content">
-	<h2>Ajouter une note</h2>		
-	<form action="gestion.php" method="post">
+	<h2>Ajouter une note</h2>
+	<form action="backend/interactDB.php" method="post">
 		<fieldset>
 			<label>Eleve</label>
 			<select name="eleve">
-			<?php
-			require("./backend/connectDB.php");
-			$request = "SELECT prenom FROM utilisateurs WHERE isProf = 0"; 
-			$resultat =mysqli_query($connexion,$request); //Executer la requete	
-			while($row = mysqli_fetch_assoc($resultat)){
-				$nom = $row['prenom'];
-				$id = $row['id'];
-				echo "<option value='$id'>$nom</option>";
-			}
-			?>
+			<?php getSelect_student(); ?>
+			</select>
+			<br>
+			<label>Matière</label>
+			<select name="matiere">
+			<?php getSelect_matiere(); ?>
+			</select>
+			<br>
+			<label>Note</label>
+			<input type="number" name="note" min=0 max=20>
 		</fieldset>
+		<input type="submit" value='Ajouter'>
+
 	</form>
 			
 </div>
---
+++ backend/interactDB.php
@@ -0,0 +1,60 @@
+<?php
+
+function getSelect_student(){
+    require("connectDB.php");
+    $request = "SELECT id,prenom FROM utilisateurs WHERE isProf = 0"; 
+    $resultat =mysqli_query($connexion,$request); //Executer la requete	
+    while($row = mysqli_fetch_assoc($resultat)){
+        $nom = $row['prenom'];
+        $id = $row['id'];
+        echo "<option value='$id'>$nom</option>";
+    }
+}
+
+function getSelect_matiere(){
+    require("connectDB.php");
+    $request = "SELECT matiereID as ID,nomMatiere FROM matiere"; 
+    $resultat =mysqli_query($connexion,$request); //Executer la requete
+    if(mysqli_num_rows($resultat) != 0) {
+        while($row = mysqli_fetch_assoc($resultat)){
+            $matiere = $row['nomMatiere'];
+            $id = $row['ID'];
+            echo "<option value='$id'>$matiere</option>";
+        }
+    }
+}
+
+function addAbsence(){
+    $idEleve = $_POST['eleve'];
+    $idMatiere = $_POST['matiere'];
+    $heure = $_POST['duree'];
+    $date = date('Y-m-d H:i:s', strtotime($_POST['date']));
+    // $date = $_POST['date'];
+    echo $date;
+
+    // $date = strval($date);
+    require("connectDB.php");
+    $request ="INSERT INTO absences (idEleve,idMatiere,duree,dateObj) VALUES ($idEleve,$idMatiere,$heure,$date)";
+    $resultat =mysqli_query($connexion,$request); //Executer la requete
+    // header('location:../ajouterAbsence.php');
+}
+
+function addNote(){
+    $idEleve = $_POST['eleve'];
+    $idMatiere = $_POST['matiere'];
+    $note = $_POST['note'];
+    require("connectDB.php");
+    $request ="INSERT INTO notes (idEleve,idMatiere,note) VALUES ($idEleve,$idMatiere,$note)";
+    $resultat =mysqli_query($connexion,$request); //Executer la requete
+    header('location:../ajouterNote.php');
+}
+
+if($_SERVER["REQUEST_METHOD"] == 'POST') { 
+    if(isset($_POST['note'])){
+        addNote();
+    }
+    if(isset($_POST['duree'])){
+        addAbsence();
+    }
+}
+?>
